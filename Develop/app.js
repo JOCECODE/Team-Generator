ebae6d055b93fe5c6f4aec0a24ffce174c9da5d0
JOCECODE <ulah57@gmail.com>
2020-09-09 23:50:52 -0700

    added recursive and made it worked

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const employeeArray = [];
-
+const employeeChoices = ["Intern", "Engineer", "No more, This is my Team!"]
+let choice = ""
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 function buildYourManager(){
     inquirer.prompt([{
         type: "input",
@@ -23,13 +25,13 @@ function buildYourManager(){
     {
         type: "input",
         name: "id",
-        message: "What is his I.D. number?",
+        message: "What is their I.D. number?",
         
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email?",
+        message: "What is their email?",
     },
     {
         type: "input",
@@ -42,11 +44,111 @@ function buildYourManager(){
             manager.email = response.email;
             manager.officeNumber = response.officeNumber;
             employeeArray.push(manager);
-            console.log(employeeArray)
-            return;
-        })};
+           buildYourTeam();
+        })}; 
+        
+function buildYourTeam() {
+    inquirer.prompt([{
+        type: "list",
+        name: "teamChoice",
+        message: "Build your team",
+        choices: employeeChoices,
+    }]).then((teamR) => {
+        choice = teamR.teamChoice;
+        //    IF USER SELECTS INTERN DO SET OF PROMPTS
+        if (choice == employeeChoices[0]) {
+            function buildYourIntern() {
+                inquirer.prompt([{
+                    type: "input",
+                    name: "nameInt",
+                    message: "What is your Intern's name?",
 
-buildYourManager();
+                },
+                {
+                    type: "input",
+                    name: "idInt",
+                    message: "What is their I.D. number?",
+
+                },
+                {
+                    type: "input",
+                    name: "emailInt",
+                    message: "What is their email?",
+                },
+                {
+                    type: "input",
+                    name: "schoolInt",
+                    message: "What is their school name?",
+                }]).then((intR) => {
+                    let intern = new Intern();
+                    intern.name = intR.nameInt;
+                    intern.id = intR.idInt;
+                    intern.email = intR.emailInt;
+                    intern.school = intR.schoolInt;
+                    employeeArray.push(intern);
+                    buildYourTeam();
+                })
+            }
+            buildYourIntern();      
+        }
+        else if (choice === employeeChoices[1]) {
+            function buildYourEngineer() {
+                inquirer.prompt([{
+                    type: "input",
+                    name: "nameEng",
+                    message: "What is your Engineer's name?",
+
+                },
+                {
+                    type: "input",
+                    name: "idEng",
+                    message: "What is their I.D. number?",
+
+                },
+                {
+                    type: "input",
+                    name: "emailEng",
+                    message: "What is their email?",
+                },
+                {
+                    type: "input",
+                    name: "githubEng",
+                    message: "What is their gitHub email?",
+                }]).then((engR) => {
+                    let engineer = new Engineer();
+                    engineer.name = engR.nameEng;
+                    engineer.id = engR.idEng;
+                    engineer.email = engR.emailEng;
+                    engineer.github = engR.githubEng;
+                    employeeArray.push(engineer);
+                    console.log(employeeArray);
+                    buildYourTeam();
+                })
+            }
+            buildYourEngineer();
+        
+        }
+        else {
+            const data = render(employeeArray);
+            fs.writeFile(outputPath, data, function () {
+                console.log("Success");
+                console.log(choice)
+            });
+        }
+    })
+} 
+         buildYourManager();
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
